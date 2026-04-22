81542885df06be6f2ebc438ad9d26d03461acfc6
KimThanh1801 <hokimthanh1234@gmail.com>
2026-04-22 13:49:25 +0700

    feat/auth - add file config/cors.php

--- config/cors.php
+++ config/cors.php
@@ -2,27 +2,14 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => [
-        'http://localhost:5173',
-        'fe-ciiclo.vercel.app',
-    ],
+    'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'https://fe-ciiclo.vercel.app'
+    ],
 
     'allowed_origins_patterns' => [],
 
@@ -33,5 +20,4 @@ return [
     'max_age' => 0,
 
     'supports_credentials' => false,
-
 ];
