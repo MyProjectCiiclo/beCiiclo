4757f366e2ba2fa9c291958d99b04b05424ba8c3
KimThanh1801 <hokimthanh1234@gmail.com>
2026-04-28 12:10:23 +0700

    fix: auth jwt + config middleware + debug 401

--- app/Http/Middleware/JWTMiddleware.php
+++ app/Http/Middleware/JWTMiddleware.php
@@ -3,30 +3,28 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Exception;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
+use GuzzleHttp\Psr7\Request;
 use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 
 class JWTMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
         try {
-            $user = auth('api')->user(); // 🔥 dòng quan trọng
-            Log::info([
-                'header' => $request->header('Authorization'),
-                'user' => $user
-            ]);
+            $user = JWTAuth::parseToken()->authenticate();
+
             if (!$user) {
-                return response()->json([
-                    'message' => 'Unauthorized'
-                ], 401);
+                return response()->json(['message' => 'Unauthorized'], 401);
             }
-        } catch (\Exception $e) {
-            return response()->json([
-                'message' => 'Unauthorized'
-            ], 401);
+        } catch (TokenExpiredException $e) {
+            return response()->json(['message' => 'Token expired'], 401);
+        } catch (TokenInvalidException $e) {
+            return response()->json(['message' => 'Token invalid'], 401);
+        } catch (JWTException $e) {
+            return response()->json(['message' => 'Token not found'], 401);
         }
 
         return $next($request);
