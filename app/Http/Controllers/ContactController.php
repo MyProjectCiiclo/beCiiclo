c9d035b93b2eb62c287431803c69260bbccd6884
KimThanh1801 <hokimthanh1234@gmail.com>
2026-05-04 15:07:39 +0700

    feat(contact): implement contact api

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreContactRequest;
+use App\Services\ContactService;
+
+class ContactController extends Controller
+{
+
+protected $contactService;
+public function __construct(ContactService $contactService){
+    $this->contactService = $contactService;
+
+}
+    public function store(StoreContactRequest $request)
+{
+    $contact = $this->contactService->sendMessage(
+        $request->validated()
+    );
+
+    return response()->json([
+        'success' => true,
+        'message' => 'Message sent successfully',
+        'data' => $contact
+    ]);
+}
+}
