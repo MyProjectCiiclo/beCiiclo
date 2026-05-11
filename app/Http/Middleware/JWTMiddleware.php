f06a9bab00c2cd2a5a8e6fcb5dfe2d30419911e8
KimThanh1801 <hokimthanh1234@gmail.com>
2026-05-11 10:44:14 +0700

    fix jwt middleware parse token

--- app/Http/Middleware/JWTMiddleware.php
+++ app/Http/Middleware/JWTMiddleware.php
@@ -8,23 +8,40 @@ use Tymon\JWTAuth\Facades\JWTAuth;
 
 class JwtMiddleware
 {
+    // public function handle(Request $request, Closure $next)
+    // {
+    //     try {
+    //         $token = $request->bearerToken();
+
+    //         if (!$token) {
+    //             return response()->json([
+    //                 'message' => 'Token not found'
+    //             ], 401);
+    //         }
+
+    //         JWTAuth::setToken($token);
+
+    //         $user = JWTAuth::authenticate();
+
+    //         if (!$user) {
+    //             return response()->json([
+    //                 'message' => 'Unauthorized'
+    //             ], 401);
+    //         }
+    //     } catch (\Throwable $e) {
+    //         return response()->json([
+    //             'message' => 'Auth error',
+    //             'error' => $e->getMessage()
+    //         ], 401);
+    //     }
+
+    //     return $next($request);
+    // }
+
     public function handle(Request $request, Closure $next)
     {
-        try {
-            $user = JWTAuth::parseToken()->authenticate();
-
-            if (!$user) {
-                return response()->json([
-                    'message' => 'Unauthorized'
-                ], 401);
-            }
-
-            return $next($request);
-        } catch (\Throwable $e) {
-            return response()->json([
-                'message' => 'Auth error',
-                'error' => $e->getMessage()
-            ], 401);
-        }
+        return response()->json([
+            'message' => 'jwt middleware reached'
+        ]);
     }
 }
