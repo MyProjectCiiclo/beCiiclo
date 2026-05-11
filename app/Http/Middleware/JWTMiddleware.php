7ea74ec454e348d1ad78c60e0db478b0dde701ff
KimThanh1801 <hokimthanh1234@gmail.com>
2026-05-11 10:49:57 +0700

    test jwt parse token

--- app/Http/Middleware/JWTMiddleware.php
+++ app/Http/Middleware/JWTMiddleware.php
@@ -8,40 +8,19 @@ use Tymon\JWTAuth\Facades\JWTAuth;
 
 class JwtMiddleware
 {
-    // public function handle(Request $request, Closure $next)
-    // {
-    //     try {
-    //         $token = $request->bearerToken();
-
-    //         if (!$token) {
-    //             return response()->json([
-    //                 'message' => 'Token not found'
-    //             ], 401);
-    //         }
-
-    //         JWTAuth::setToken($token);
-
-    //         $user = JWTAuth::authenticate();
-
-    //         if (!$user) {
-    //             return response()->json([
-    //                 'message' => 'Unauthorized'
-    //             ], 401);
-    //         }
-    //     } catch (\Throwable $e) {
-    //         return response()->json([
-    //             'message' => 'Auth error',
-    //             'error' => $e->getMessage()
-    //         ], 401);
-    //     }
-
-    //     return $next($request);
-    // }
-
     public function handle(Request $request, Closure $next)
     {
-        return response()->json([
-            'message' => 'jwt middleware reached'
-        ]);
+        try {
+            $token = JWTAuth::parseToken();
+
+            return response()->json([
+                'message' => 'token parsed'
+            ]);
+        } catch (\Throwable $e) {
+            return response()->json([
+                'message' => 'Auth error',
+                'error' => $e->getMessage()
+            ], 401);
+        }
     }
 }
