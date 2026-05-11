368c55d692124467351d752bb3b4f4af10096420
KimThanh1801 <hokimthanh1234@gmail.com>
2026-05-11 10:00:18 +0700

    register jwt middleware alias in bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,11 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {})
+    ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->alias([
+            'jwt' => \App\Http\Middleware\JwtMiddleware::class,
+        ]);
+    })
     ->withExceptions(function (Exceptions $exceptions): void {
 
         $exceptions->render(function (AuthenticationException $e) {
